df1f635b499fce8a2d99f5379729b017e957f5fc
AntonioAssat <turcotomas280302@gmail.com>
2026-08-13 17:40:14 -0300

    Primer commit con la estructura MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AnaYAntonio_ProyectoInmobiliaria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
