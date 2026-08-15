64267f88f08edc71c29ca11b51239d8b0d6f0788
quevedoana <et.quevedoana@gmail.com>
2026-08-15 19:04:37 -0300

    feat: interfaz generica

---
+++ Models/IRepositorio.cs
@@ -0,0 +1,9 @@
+namespace AnaYAntonio_ProyectoInmobiliaria.Models
+{
+    public interface IRepositorio<T>
+    {
+        int Alta(T p);
+        int Baja(int id);
+        int Modificacion(T p);
+    }
+}
