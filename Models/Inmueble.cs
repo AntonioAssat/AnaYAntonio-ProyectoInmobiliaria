367735e304c297ec6468fdfcc6da81543eeb2ca7
quevedoana <et.quevedoana@gmail.com>
2026-09-01 17:06:47 -0300

    feat: modelo Inmueble

---
+++ Models/Inmueble.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnaYAntonio_ProyectoInmobiliaria.Models
+{
+    public class Inmueble
+    {
+        [Key]
+        public int ID_inmueble { get; set; }
+
+        [Required]
+        public int ID_propietario { get; set; }
+
+        [Required]
+        public string Direccion { get; set; } = "";
+
+        [Required]
+        public int Cupo { get; set; }
+
+        [Required]
+        public int ID_tipo { get; set; }
+
+        [Required]
+        public decimal Coordenadas { get; set; }
+
+        [Required]
+        public decimal PrecioPorDia { get; set; }
+
+        [Required]
+        public decimal PorcentajeReserva { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
